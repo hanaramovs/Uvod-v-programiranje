8ed76be731b769d57044bf31c7af89fce1a3a444
hanaramovs <hanna.ramovs@gmail.com>
2024-10-18 20:13:18 +0200

    sprememba izbora spletne strani, dodatek osnutka kode za csv in shranjevanje html datotek iz spleta

---
+++ podatki_zajem.py
@@ -0,0 +1,18 @@
+import podatki_urejanje as vzorec
+
+
+def main(redownload=True):
+    for i in range(50):
+        link = f'https://properties.lefigaro.com/announces/bastide-real+estate-properties+for+sale-france/?type_bien=6&type_bien=9&type_bien=38&type_bien=37&type_bien=34&prix_max=1000000&surface_terrain_min=200&nb_chambres_min=1&nb_pieces_min=1&page={i + 1}'
+        if (redownload):
+            vzorec.shrani(link, 'oglasi_html', f'oglasi_{i + 1}.html')
+    text = ''
+    for i in range(50):
+        text += vzorec.datoteka_v_niz('oglasi.html', f'oglasi_{i + 1}.html')
+    oglasi = vzorec.seznam_oglasov(text)
+    seznam = [vzorec.slovar_pojmov_v_oglasu(oglas) for oglas in oglasi]
+    vzorec.oglasi_v_csv(seznam, 'projektna_naloga_nepremicnine', 'oglasi.csv')
+
+if __name__ == '__main__':
+    main()
+
